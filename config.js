a993235788c295f981ea9f066583d5911e885a2d
FukudaApplication <rokukoha6958@yahoo.co.jp>
2026-01-25 15:16:35 +0900

    Add Authentification Function

---
+++ config.js
@@ -0,0 +1,8 @@
+// Supabase設定ファイル
+const SUPABASE_CONFIG = {
+  // SupabaseプロジェクトのURL（ダッシュボードから取得）
+  url: "https://narfojqlqqresvuxgpeo.supabase.co",
+
+  // Supabaseプロジェクトの公開APIキー（anon key）
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcmZvanFscXFyZXN2dXhncGVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MTAyNDEsImV4cCI6MjA4NDI4NjI0MX0.4MKjRCYiqpHcT6C9hyVcA4u1uEyZVRhbh3_R9UUs9kU",
+};
